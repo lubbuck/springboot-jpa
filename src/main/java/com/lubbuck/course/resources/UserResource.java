77a40351874ec59be8c3ff46f576144ec167d5c6
Antônio Luís <antonioluisp97@gmail.com>
2024-01-10 20:43:34 -0300

    feat camada de servico do user

--- src/main/java/com/lubbuck/course/resources/UserResource.java
+++ src/main/java/com/lubbuck/course/resources/UserResource.java
@@ -1,21 +1,34 @@
 package com.lubbuck.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lubbuck.course.entitier.User;
+import com.lubbuck.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "antonio", "antonio@gmail.com", "98988210510", "123456789");
+	@Autowired
+	private UserService userService;
 
-		return ResponseEntity.ok().body(u);
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users = userService.findAll();
+		return ResponseEntity.ok().body(users);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 
 }
