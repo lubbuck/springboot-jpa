92280494111b8d2bd72bd7bd109da40167396afa
Antônio Luís <antonioluisp97@gmail.com>
2024-01-11 18:09:17 -0300

    feat: product entity repository service resource e relacionamento com categories

--- src/main/java/com/lubbuck/course/config/TestConfig.java
+++ src/main/java/com/lubbuck/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.lubbuck.course.entities.Category;
 import com.lubbuck.course.entities.Order;
+import com.lubbuck.course.entities.Product;
 import com.lubbuck.course.entities.User;
 import com.lubbuck.course.entities.enums.OrderStatus;
 import com.lubbuck.course.repositories.CategoryRepository;
 import com.lubbuck.course.repositories.OrderRepository;
+import com.lubbuck.course.repositories.ProductRepository;
 import com.lubbuck.course.repositories.UserRepository;
 
 @Configuration
@@ -28,6 +30,9 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
+
+	@Autowired
+	private ProductRepository produtcRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -48,5 +53,23 @@ public class TestConfig implements CommandLineRunner {
 		Category cat3 = new Category(null, "Computers");
 
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
+		produtcRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		produtcRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 	}
 }
--- src/main/java/com/lubbuck/course/entities/Category.java
+++ src/main/java/com/lubbuck/course/entities/Category.java
@@ -1,13 +1,19 @@
 package com.lubbuck.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +25,10 @@ public class Category implements Serializable {
 	private Long id;
 
 	private String name;
+
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 
 	public Category() {
 	}
@@ -44,6 +54,10 @@ public class Category implements Serializable {
 		this.name = name;
 	}
 
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -60,5 +74,4 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-
 }
