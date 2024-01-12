0310f40594b65593f8c93877d1b8f4bf55869682
Antônio Luís <antonioluisp97@gmail.com>
2024-01-12 18:38:18 -0300

    feat exception para delete e update de user

--- src/main/java/com/lubbuck/course/services/UserService.java
+++ src/main/java/com/lubbuck/course/services/UserService.java
@@ -4,11 +4,15 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.lubbuck.course.entities.User;
 import com.lubbuck.course.repositories.UserRepository;
+import com.lubbuck.course.services.exceptions.DatabaseException;
 import com.lubbuck.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -30,9 +34,13 @@ public class UserService {
 	}
 
 	public User update(Long id, User user) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, user);
-		return userRepository.save(entity);
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, user);
+			return userRepository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User user) {
@@ -42,6 +50,14 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		if (!userRepository.existsById(id)) {
+			throw new ResourceNotFoundException(id);
+		}
+		try {
+			userRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+
 	}
 }
